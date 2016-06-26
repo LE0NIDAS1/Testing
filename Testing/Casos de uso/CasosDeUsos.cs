e8bb27aa8df22c2e42a0170c47ef7243d3a25927
leoeg <leoeg@DESKTOP-5BFAJNT>
2016-06-26 02:21:52 -0300

    Selenium y NUnit

---
+++ Testing/Casos de uso/CasosDeUsos.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Casos_de_uso
+{
+    class CasosDeUsos
+    {
+        [SetUp]
+        public void initialize(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("https://localhost:44300/");
+        }
+
+        [Test]
+        public void login(IWebDriver driver)
+        {
+            IWebElement element = driver.FindElement(By.Id("loginLink"));
+            element.Click();
+            IWebElement email = driver.FindElement(By.Id("Email"));
+            email.SendKeys("Admin@atlas.com");
+            IWebElement pass = driver.FindElement(By.Id("Password"));
+            pass.SendKeys("Atlas@123");
+            IWebElement btn = driver.FindElement(By.Id("save"));
+            btn.Submit();
+        }
+
+        [TearDown]
+        public void cleanUp(IWebDriver driver)
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testing.Casos_de_uso;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string algo = Environment.CurrentDirectory;
+
+            IWebDriver driver = new ChromeDriver(algo.Substring(0, algo.IndexOf("bin")));
+
+            CasosDeUsos casos = new CasosDeUsos();
+            casos.initialize(driver);
+            //Iniciar sesion
+            casos.login(driver);
+            casos.cleanUp(driver);
+
+        }
+    }
+}
